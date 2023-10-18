be4c0e011a5066ed9b17ae95708304d799812cf9
AndreElik <elistratov_andre@mail.ru>
2023-10-18 16:30:06 +0300

    Причесал функцию и добавил документационную строку

--- post_v1_account.py
+++ post_v1_account.py
@@ -1,20 +1,31 @@
 import requests
 import json
 
-url = "http://5.63.153.31:5051/v1/account"
 
-payload = json.dumps({
-  "login": "user_253",
-  "email": "user_253@gmail.com",
-  "password": "123456qwerty"
-})
-headers = {
-  'X-Dm-Auth-Token': '<string>',
-  'X-Dm-Bb-Render-Mode': '<string>',
-  'Content-Type': 'application/json',
-  'Accept': 'text/plain'
-}
+def post_v1_account():
+    """
+    Register new user
+    :return:
+    """
+    url = "http://5.63.153.31:5051/v1/account"
 
-response = requests.request("POST", url, headers=headers, data=payload)
+    payload = {
+      "login": "user_253",
+      "email": "user_253@gmail.com",
+      "password": "123456qwerty"
+    }
+    headers = {
+      'X-Dm-Auth-Token': '<string>',
+      'X-Dm-Bb-Render-Mode': '<string>',
+      'Content-Type': 'application/json',
+      'Accept': 'text/plain'
+    }
 
-print(response.text)
+    response = requests.request(
+      method="POST",
+      url=url,
+      headers=headers,
+      json=payload)
+
+    return response
+
