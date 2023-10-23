452b757483009b307261639964d9853c525df4bf
AndreElik <elistratov_andre@mail.ru>
2023-10-23 21:42:16 +0300

    добавил тест  test_put_v1_account

--- tests/test_put_v1_account.py
+++ tests/test_put_v1_account.py
@@ -1,4 +1,11 @@
 import requests
+from serviceces.dm_api_account import DmApiAccount
 
+
+def test_put_v1_account():
+    api = DmApiAccount(host='http://5.63.153.31:5051')
+    token = '564e9e0e-3232-4f00-ac72-056f616aabd6'
+    response = api.account.put_v1_account(token=token)
+    print(response)
 
 
